197af8f9b588fb691e806eee1a294dbd695c02be
AvivSham <Mista2311@gmail.com>
2020-01-05 16:31:14 +0200

    init

---
+++ VAE.py
@@ -0,0 +1,76 @@
+"""NICE model
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class Model(nn.Module):
+    def __init__(self, latent_dim,device):
+        """Initialize a VAE.
+
+        Args:
+            latent_dim: dimension of embedding
+            device: run on cpu or gpu
+        """
+        super(Model, self).__init__()
+        self.device = device
+        self.latent_dim = latent_dim
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 32, 4, 1, 2),  # B,  32, 28, 28
+            nn.ReLU(True),
+            nn.Conv2d(32, 32, 4, 2, 1),  # B,  32, 14, 14
+            nn.ReLU(True),
+            nn.Conv2d(32, 64, 4, 2, 1),  # B,  64,  7, 7
+        )
+
+        self.mu = nn.Linear(64 * 7 * 7, latent_dim)
+        self.logvar = nn.Linear(64 * 7 * 7, latent_dim)
+
+        self.upsample = nn.Linear(latent_dim, 64 * 7 * 7)
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(64, 32, 4, 2, 1),  # B,  64,  14,  14
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, 32, 4, 2, 1, 1),  # B,  32, 28, 28
+            nn.ReLU(True),
+            nn.ConvTranspose2d(32, 1, 4, 1, 2),  # B, 1, 28, 28
+            nn.Sigmoid()
+        )
+
+    def sample(self,sample_size=None, mu=None, logvar=None):
+        """
+        :param sample_size: Number of samples
+        :param mu: z mean, None for init with zeros
+        :param logvar: z logstd, None for init with zeros
+        :return:
+        """
+        if mu==None:
+            mu = torch.zeros((sample_size,self.latent_dim)).to(self.device)
+        if logvar == None:
+            logvar = torch.zeros((sample_size,self.latent_dim)).to(self.device)
+            sample_img = torch.randn(sample_size,self.latent_dim) * logvar + mu
+            sample_img = self.decoder(self.upsample(sample_img)).cpu()
+        return sample_img
+
+
+    def z_sample(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.rand_like(std)
+        return mu + eps * std
+
+    @staticmethod
+    def loss(x, recon, mu, logvar):
+        # Binary cross entropy loss
+        BCE = F.binary_cross_entropy(recon, x.view(-1, 784), reduction='sum')
+        # KL Divergence loss
+        KL = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+        return BCE + KL
+
+    def forward(self, x):
+        x_latent = self.encoder(x).view(-1,64*7*7)
+        mu, logvar = self.mu(x_latent), self.logvar(x_latent)
+        z = self.z_sample(mu=mu, logvar=logvar)
+        return self.decoder(self.upsample(z).view(-1,64,7,7)), mu, logvar
---
+++ train.py
@@ -0,0 +1,121 @@
+"""Training procedure for NICE.
+"""
+
+import argparse
+import torch, torchvision
+from torchvision import transforms
+from torchvision.utils import save_image, make_grid
+import numpy as np
+import VAE
+import pickle
+
+N = 100
+
+
+def train(vae : VAE.Model, trainloader, optimizer, epoch, device):
+    vae.train()  # set to training mode
+    batch_loss = 0
+    for batch_idx, (inputs, _) in enumerate(trainloader):
+        inputs = inputs.to(device)
+        optimizer.zero_grad()
+        x_recon, mu, logvar = vae(inputs)
+        loss = vae.loss(inputs.view(-1,784), x_recon.view(-1,784), mu=mu, logvar=logvar)
+        loss.backward()
+        batch_loss += loss.item()
+        optimizer.step()
+    return batch_loss / len(trainloader), mu, logvar
+
+
+def test(vae: VAE.Model, testloader, device: torch.device):
+    vae.eval()  # set to inference mode
+    with torch.no_grad():
+        batch_loss = 0
+        for batch_idx, (inputs, _) in enumerate(testloader):
+            inputs = inputs.to(device)
+            x_recon, mu, logvar = vae(inputs)
+            loss = vae.loss(inputs, x_recon, mu=mu, logvar=logvar)
+            batch_loss += loss.item()
+        return batch_loss / len(testloader)
+
+
+
+def main(args):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    transform  = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x + torch.zeros_like(x).uniform_(0., 1./256.)), #dequantization
+        transforms.Normalize((0.,), (257./256.,)), #rescales to [0,1]
+
+    ])
+
+    if args.dataset == 'mnist':
+        trainset = torchvision.datasets.MNIST(root='./data/MNIST',
+            train=True, download=True, transform=transform)
+        trainloader = torch.utils.data.DataLoader(trainset,
+            batch_size=args.batch_size, shuffle=True, num_workers=0)
+        testset = torchvision.datasets.MNIST(root='./data/MNIST',
+            train=False, download=True, transform=transform)
+        testloader = torch.utils.data.DataLoader(testset,
+            batch_size=args.batch_size, shuffle=False, num_workers=0)
+    elif args.dataset == 'fashion-mnist':
+        trainset = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST',
+            train=True, download=True, transform=transform)
+        trainloader = torch.utils.data.DataLoader(trainset,
+            batch_size=args.batch_size, shuffle=True, num_workers=0)
+        testset = torchvision.datasets.FashionMNIST(root='./data/FashionMNIST',
+            train=False, download=True, transform=transform)
+        testloader = torch.utils.data.DataLoader(testset,
+            batch_size=args.batch_size, shuffle=False, num_workers=0)
+    else:
+        raise ValueError('Dataset not implemented')
+
+    filename = '%s_' % args.dataset \
+             + 'batch%d_' % args.batch_size \
+             + 'mid%d_' % args.latent_dim
+
+    vae = VAE.Model(latent_dim=args.latent_dim,device=device).to(device)
+    optimizer = torch.optim.Adam(
+        vae.parameters(), lr=args.lr)
+
+    elbo_train, elbo_val = [], []
+    for epoch in range(args.epochs):
+        train_loss, mu, logvar = train(vae=vae, trainloader=trainloader, optimizer=optimizer,
+                                       epoch=epoch, device=device)
+        elbo_train.append(train_loss)
+        elbo_val.append(test(vae=vae, testloader=testloader,device=device))
+    save_image(make_grid(vae.sample(args.sample_size, mu=mu, logvar=logvar), "output.jpg"),
+               filename=filename)
+    print("running done")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('')
+    parser.add_argument('--dataset',
+                        help='dataset to be modeled.',
+                        type=str,
+                        default='mnist')
+    parser.add_argument('--batch_size',
+                        help='number of images in a mini-batch.',
+                        type=int,
+                        default=128)
+    parser.add_argument('--epochs',
+                        help='maximum number of iterations.',
+                        type=int,
+                        default=50)
+    parser.add_argument('--sample_size',
+                        help='number of images to generate.',
+                        type=int,
+                        default=64)
+
+    parser.add_argument('--latent-dim',
+                        help='.',
+                        type=int,
+                        default=100)
+    parser.add_argument('--lr',
+                        help='initial learning rate.',
+                        type=float,
+                        default=1e-3)
+
+    args = parser.parse_args()
+    main(args)
+
